f59f5ad6a530d4d1c74440ada61515711c5d4eb0
Yuriy <xgooglex@bk.ru>
2024-06-25 17:19:00 +0500

    add style.css

---
+++ app.js
@@ -0,0 +1,13 @@
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+let carouselDom = document.querySelector('.carousel');
+let listItemDom = document.querySelector('.carousel .list');
+let thumbnailDom = document.querySelector('.carousel .thumbtail');
+
+nextDom.onclick = function() {
+    showSlider('next');
+}
+
+function showSlider(type){
+    
+}
